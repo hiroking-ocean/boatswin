97499c35be048fc13a6860d03a442bc551462b05
Hiroki Tanaka <hariiniya@gmail.com>
2020-09-17 00:02:39 +0900

    Add config precompile target

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,7 @@ module Boatswin
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    Rails.application.config.assets.precompile += %w( *.eot *.woff *.ttf *.svg *.otf *.png *.jpg *.jpeg *.gif vendor.css vendor.js )
   end
 end
